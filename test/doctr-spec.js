162889a277d9a2240554d455a95ee9665fd8eb8e
Patrick Wied <patrick.wied@live.at>
2013-03-01 08:42:17 +0100

    Initial project structure

---
+++ test/doctr-spec.js
@@ -0,0 +1,27 @@
+var doctr = require('../index.js'),
+	fs = require('fs'),
+	path = require('path');
+
+describe('doctr', function() {
+
+  it('does exist', function() {
+    expect(typeof doctr).toEqual('object');
+  });
+
+  it('generates jsdoc from specified input to output folder', function() {
+
+  });
+
+  it('strips unnecessary html-header from generated jsdoc file', function() {
+
+  });
+
+  it('creates a folder `release/doc` for the generated html doc', function() {
+
+  });
+
+  it('makes `release/doc` folder available on a webserver', function() {
+
+  });
+
+});
