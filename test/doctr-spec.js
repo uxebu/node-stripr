5f6bdf1688aae58d7e2c52ce556a95fdd5d3dbdf
Patrick Wied <patrick.wied@live.at>
2013-03-01 10:35:17 +0100

    Adds the first few tests

--- test/doctr-spec.js
+++ test/doctr-spec.js
@@ -1,6 +1,7 @@
 var doctr = require('../index.js'),
-	fs = require('fs'),
-	path = require('path');
+    fs = require('fs'),
+    path = require('path'),
+    http = require('http');
 
 describe('doctr', function() {
 
@@ -8,20 +9,62 @@ describe('doctr', function() {
     expect(typeof doctr).toEqual('object');
   });
 
-  it('generates jsdoc from specified input to output folder', function() {
+  // internal implementation detail, don't care about it?
+  it('generates correct jsdoc from specified input to `tmp/jsdoc`', function() {
+
+    var mockFile = fs.readFileSync(path.join(__dirname, 'jsdoctest.html'));
+
+    var config = {
+      jsFolder: path.join(__dirname, 'jsdoctest.js'),
+      callback: function() {
+
+        var generatedFile = fs.readFileSync(path.join(__dirname, 'tmp/jsdoc/jsdoctest.html'));
+        expect(generatedFile.toString()).toBe(mockFile.toString());
+
+      }
+    };
+
+    doctr.generateJsdoc(config);
+    // TODO: cleanup.
 
   });
 
-  it('strips unnecessary html-header from generated jsdoc file', function() {
+  it('strips unnecessary html-header from generated jsdoc file in `tmp/jsdoc`', function() {
+    var mockFile = fs.readFileSync(path.join(__dirname, 'jsdoctest.md'));
+    var filename = path.join(__dirname, 'jsdoctest.html');
+
+    expect(doctr.getStrippedContent(filename)).toBe(mockFile.toString());
 
   });
 
-  it('creates a folder `release/doc` for the generated html doc', function() {
+  it('renames file to .md after stripping html-header', function() {
+    var filename = path.join(__dirname, 'jsdoctest2.html');
+    doctr.stripJsdocHeader(filename);
+
+    expect(fs.existsSync(path.join(__dirname, 'jsdoctest2.md'))).toBeTruthy();
 
   });
 
-  it('makes `release/doc` folder available on a webserver', function() {
+  it('creates a folder <folderName> for the generated html doc', function() {
+    var config = {
+      // config values
+    };
 
+    doctr.run(config);
+    expect(fs.existsSync(config.outputFolder)).toBeTruthy();
+  });
+
+  it('makes <folderName> folder available on a webserver', function(done) {
+    var config = {
+      // config values
+    };
+
+    doctr.run(config);
+
+    http.get('http://localhost:8000/' + config.webFolder, function(response) {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
   });
 
 });
