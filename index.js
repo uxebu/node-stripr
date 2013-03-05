07891f6ffc8a4c095a7cf28995ca041619198c78
Patrick Wied <patrick.wied@live.at>
2013-03-05 12:38:02 +0100

    Adds basic html stripping functionality and tests

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ var allowedInputTypes = ["number", "string"];
 
 module.exports = function(){
 
-	if(arguments.length == 0 || allowedInputTypes.indexOf(arguments[0]) == -1) {
-		return "";
+	if(arguments.length == 0 || allowedInputTypes.indexOf(typeof arguments[0]) == -1) {
+		return '';
 	}
 
   var input = arguments[0],
-      output = "";
+      inputLower = input.toLowerCase(),
+      output = input;
 
+  var stripRegexes = {
+    '<!doctype': /<!doctype((.|\n|\r)*?)>/i,
+    '<html': /<html((.|\n|\r)*?)>/i,
+    '<head': /<head>(?:.|\n|\r)+?<\/head>/i,
+    '<body': /<body((.|\n|\r)*?)>/i,
+    '</body>': /<\/body>/i,
+    '</html>': /<\/html>/i
+  };
+
+  for(var key in stripRegexes){
+    if(stripRegexes.hasOwnProperty(key)){
+
+      // check if keys have a match in input and strip away using the regexes
+      if(inputLower.indexOf(key) > -1){
+        output = output.replace(stripRegexes[key], '');
+      }
+    }
+  }
 
   return output;
 
--- test/doctr-spec.js
+++ test/doctr-spec.js
@@ -9,5 +9,14 @@ describe('doctr', function() {
   it('returns an empty string by default', function() {
     expect(doctr()).toBe('');
   });
-  
+
+  it('strips unnecessary html structure from valid HTML string', function() {
+    expect(doctr('<html><head><title>Test</head><body><div>Test</div></body></html>'))
+      .toBe('<div>Test</div>');
+  });
+
+  it('strips doctype from HTML string if existent', function() {
+    expect(doctr('<!DOCTYPE html><div>Test</div>')).toBe('<div>Test</div>');
+  });
+
 });
