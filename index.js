730ea8b9b1ff76e1a2807b8b185b913dc0413092
Patrick Wied <patrick.wied@live.at>
2013-03-06 11:51:21 +0100

    Fixes coding-style

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var allowedInputTypes = ["number", "string"];
+var allowedInputTypes = ['number', 'string'];
 
+module.exports = function() {
 
-module.exports = function(){
-
-	if(arguments.length == 0 || allowedInputTypes.indexOf(typeof arguments[0]) == -1) {
-		return '';
-	}
+  if (arguments.length == 0 || allowedInputTypes.indexOf(typeof arguments[0]) == -1) {
+    return '';
+  }
 
   var input = arguments[0],
       inputLower = input.toLowerCase(),
@@ -20,11 +19,11 @@ module.exports = function(){
     '</html>': /<\/html>/i
   };
 
-  for(var key in stripRegexes){
-    if(stripRegexes.hasOwnProperty(key)){
+  for (var key in stripRegexes) {
+    if (stripRegexes.hasOwnProperty(key)) {
 
       // check if keys have a match in input and strip away using the regexes
-      if(inputLower.indexOf(key) > -1){
+      if (inputLower.indexOf(key) > -1) {
         output = output.replace(stripRegexes[key], '');
       }
     }
