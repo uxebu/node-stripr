07f98c2627448a473d6046c1a025b9ab292893e6
Patrick Wied <patrick.wied@live.at>
2013-03-13 17:37:00 +0100

    Renames test-spec and variables

--- test/stripr-spec.js
+++ test/stripr-spec.js
@@ -1,50 +1,50 @@
-var doctr = require('../index.js');
+var stripr = require('../index.js');
 
-describe('doctr', function() {
+describe('stripr', function() {
 
   it('does exist', function() {
-    expect(typeof doctr).toEqual('function');
+    expect(typeof stripr).toEqual('function');
   });
 
   it('returns an empty string by default', function() {
-    expect(doctr()).toBe('');
+    expect(stripr()).toBe('');
   });
 
   it('strips unnecessary html structure from valid HTML string', function() {
-    expect(doctr('<html><head><title>Test</head><body><div>Test</div></body></html>'))
+    expect(stripr('<html><head><title>Test</head><body><div>Test</div></body></html>'))
       .toBe('<div>Test</div>');
   });
 
   it('strips doctype from HTML string if existent', function() {
-    expect(doctr('<!DOCTYPE html><div>Test</div>')).toBe('<div>Test</div>');
+    expect(stripr('<!DOCTYPE html><div>Test</div>')).toBe('<div>Test</div>');
   });
 
   it('strips doctype and unnecessary tags from valid HTML string with CSS classes', function() {
-    expect(doctr('<!DOCTYPE html><html class="ie7"><head><title>Test</head><body class="ie7"><div>Test</div></body></html>'))
+    expect(stripr('<!DOCTYPE html><html class="ie7"><head><title>Test</head><body class="ie7"><div>Test</div></body></html>'))
       .toBe('<div>Test</div>');
   });
 
   it('strips unnecessary tags from valid HTML string with uppercase-only coding style', function() {
-    expect(doctr('<HTML><HEAD><TITLE>Test!</TITLE></HEAD><BODY><DIV>Test</DIV></BODY></HTML>'))
+    expect(stripr('<HTML><HEAD><TITLE>Test!</TITLE></HEAD><BODY><DIV>Test</DIV></BODY></HTML>'))
       .toBe('<DIV>Test</DIV>');
   });
 
   it('strips doctype and unnecessary tags from valid HTML string with multiline syntax', function() {
-    expect(doctr('<html \
+    expect(stripr('<html \
       class="ie7"><head><title>Test \
       </title></head><body><div>Test</div></body></html>')).toBe('<div>Test</div>');
   });
 
   it('doesn\'t strip anything if the input is a valid HTML fragment', function() {
-    expect(doctr('Test<br /><div>Test</div>')).toBe('Test<br /><div>Test</div>');
+    expect(stripr('Test<br /><div>Test</div>')).toBe('Test<br /><div>Test</div>');
   });
 
   it('strips unnecessary html structure including empty head element edge case from valid HTML string', function() {
-    expect(doctr('<html><head /><body><div>Test</div></body></html>')).toBe('<div>Test</div>');
+    expect(stripr('<html><head /><body><div>Test</div></body></html>')).toBe('<div>Test</div>');
   });
 
   it('strips unnecessary html structure based on optionally provided stripping rules', function() {
-    expect(doctr('<html><head /><body><nav>Test</nav><div>Test</div></body></html>', {
+    expect(stripr('<html><head /><body><nav>Test</nav><div>Test</div></body></html>', {
       '<nav': /<nav(?:.|\n|\r)+?(<\/nav>|\/>)/i
     })).toBe('<div>Test</div>');
   });
